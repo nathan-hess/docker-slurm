1874bc8360417668220b277395959305a89afa89
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-05-08 14:30:34 -0400

    Slurm v21 configuration changes
    Previously, Docker image setup had been performed for containers based on Ubuntu 20.04 with Slurm v19 installed. In feb1259a61f386d196cfd197ee42470b684bfa58, the base operating system version was updated to Ubuntu 22.04 and Slurm v21 was installed. Several Slurm defaults changed when updated, requiring configuration changes

--- dockerfile_base/startup.sh
+++ dockerfile_base/startup.sh
@@ -10,8 +10,8 @@ fi
 # Configure Slurm to use maximum available processors and memory
 # and start required services
 ${sudo_cmd} bash <<SCRIPT
-sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm-llnl/slurm.conf
-sed -i "s/<<MEMORY>>/$(awk '/MemTotal/ { printf "%.0f \n", $2/1024 }' /proc/meminfo)/" /etc/slurm-llnl/slurm.conf
+sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm/slurm.conf
+sed -i "s/<<MEMORY>>/$(awk '/MemTotal/ { printf "%.0f \n", $2/1024 }' /proc/meminfo)/" /etc/slurm/slurm.conf
 service munge start
 service slurmd start
 service slurmctld start
