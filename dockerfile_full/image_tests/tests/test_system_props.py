0c2d6c0f2ce0dcd7243e467dd1b1bcd7761d2565
Nathan Hess <63890205+nathan-hess@users.noreply.github.com>
2022-05-16 01:31:15 -0400

    Add test for timezone
    Added a check to ensure that time zone is set to `America/New_York`

--- dockerfile_full/image_tests/tests/test_system_props.py
+++ dockerfile_full/image_tests/tests/test_system_props.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 
 from .utils import check_pkg_exists
@@ -7,7 +8,7 @@ from .utils import check_pkg_exists
 class TestSystem(unittest.TestCase):
     def test_os_version(self):
         # Checks that operating system is Ubuntu 22.04.X
-        with open('/etc/os-release') as fileID:
+        with open('/etc/os-release', 'r', encoding='UTF-8') as fileID:
             os_release_file = {
                 i.split('=')[0].strip(): i.split('=')[1].strip().strip('"')
                 for i in fileID.readlines()
@@ -15,6 +16,14 @@ class TestSystem(unittest.TestCase):
         self.assertEqual(os_release_file['ID'], 'ubuntu')
         self.assertEqual(os_release_file['VERSION_ID'].split('.')[0], '22')
         self.assertEqual(os_release_file['VERSION_ID'].split('.')[1], '04')
+
+    def test_time_zone(self):
+        # Checks that timezone is set to `America/New_York`
+        self.assertEqual(time.tzname[0], 'EST')
+
+        with open('/etc/timezone', 'r', encoding='UTF-8') as fileID:
+            timezone = fileID.read()
+        self.assertEqual(timezone.strip(), 'America/New_York')
 
 
 class TestSystemRoot(unittest.TestCase):
